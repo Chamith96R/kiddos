dfbad1298afb7bac24ab0ef646315d569b77394f
Chamith96R <chamithrpriyadarshana@gmail.com>
2022-12-06 00:42:10 +0530

    component

---
+++ components/Section9.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Section9() {
+  return (
+    <div>
+        <div class="blog-entry">
+            <a href="" class="block-20 fimage5">
+                <div class="content-end meta-date text-center p-2">
+                    <span class="day">27</span>
+                    <span class="mos">January</span>
+                    <span class="yr">2019</span>
+                </div>
+            </a>
+            <div class="text bg-white p-5">
+                <h2 class="hover:text-sky-500 text-2xl mb-5">Skills To Develop Your Child Memory</h2>
+                <p className="text-gray-500">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                <div class="flex items-center mt-4">
+                    <p class="mb-0 mt-4"><a href="#" class="inline-block align-middle text-center select-none font-normal rounded-full py-1 px-3 leading-normal no-underline bg-orange-400 text-white hover:bg-gray-700">Read More </a></p>
+                    <p class="ml-auto mb-0">
+                        <a href="#" class="mr-2 text-sky-500">Admin</a>
+                        <a href="#" class=""> 3</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,7 @@ import Section4 from '../components/Section4';
 import Section5 from '../components/Section5';
 import Section6 from '../components/Section6';
 import Section7 from '../components/Section7';
+import Section9 from '../components/Section9';
 
 
 export default function Home() {
@@ -395,73 +396,15 @@ export default function Home() {
         </div>
         <div data-aos="fade-up" className="flex flex-wrap font1 pr-6 pl-6 pb-20">
                <div class="md:w-1/2 pr-4 pl-4 lg:w-1/3 ">
-                    <div class="blog-entry">
-                        <a href="" class="block-20 fimage5">
-                            <div class="content-end meta-date text-center p-2">
-                                <span class="day">27</span>
-                                <span class="mos">January</span>
-                                <span class="yr">2019</span>
-                            </div>
-                        </a>
-                        <div class="text bg-white p-5">
-                            <h2 class="hover:text-sky-500 text-2xl mb-5">Skills To Develop Your Child Memory</h2>
-                            <p className="text-gray-500">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                            <div class="flex items-center mt-4">
-                                <p class="mb-0 mt-4"><a href="#" class="inline-block align-middle text-center select-none font-normal rounded-full py-1 px-3 leading-normal no-underline bg-orange-400 text-white hover:bg-gray-700">Read More </a></p>
-                                <p class="ml-auto mb-0">
-                                    <a href="#" class="mr-2 text-sky-500">Admin</a>
-                                    <a href="#" class=""> 3</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    <Section9/>
                 </div>
-        
-
+    
                <div class="md:w-1/2 pr-4 pl-4 lg:w-1/3">
-                    <div class="blog-entry">
-                        <a href="" class="block-20 fimage6">
-                            <div class="meta-date text-center p-2">
-                                <span class="day">27</span>
-                                <span class="mos">January</span>
-                                <span class="yr">2019</span>
-                            </div>
-                        </a>
-                        <div class="text bg-white p-5">
-                            <h2 class="hover:text-sky-500 text-2xl mb-5">Skills To Develop Your Child Memory</h2>
-                            <p className="text-gray-500">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                            <div class="flex items-center mt-4">
-                                <p class="mb-0 mt-4"><a href="#" class="inline-block align-middle text-center select-none font-normal rounded-full py-1 px-3 leading-normal no-underline bg-orange-400 text-white hover:bg-gray-700">Read More </a></p>
-                                <p class="ml-auto mb-0">
-                                    <a href="#" class="mr-2 text-sky-500">Admin</a>
-                                    <a href="#" class=""> 3</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
+                    <Section9/>
+                </div>
 
-                    <div class="md:w-1/2 pr-4 pl-4 lg:w-1/3">
-                    <div class="blog-entry">
-                        <a href="" class="block-20 fimage7">
-                            <div class="meta-date text-center p-2">
-                                <span class="day">27</span>
-                                <span class="mos">January</span>
-                                <span class="yr">2019</span>
-                            </div>
-                        </a>
-                        <div class="text bg-white p-5">
-                            <h2 class="hover:text-sky-500 text-2xl mb-5">Skills To Develop Your Child Memory</h2>
-                            <p className="text-gray-500">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                            <div class="flex items-center mt-4">
-                                <p class="mb-0 mt-4"><a href="#" class="inline-block align-middle text-center select-none font-normal rounded-full py-1 px-3 leading-normal no-underline bg-orange-400 text-white hover:bg-gray-700">Read More</a></p>
-                                <p class="ml-auto mb-0">
-                                    <a href="#" class="mr-2 text-sky-500">Admin</a>
-                                    <a href="#" class=""> 3</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                <div class="md:w-1/2 pr-4 pl-4 lg:w-1/3">
+                    <Section9/>
                 </div>                
         </div>        
     </div>
